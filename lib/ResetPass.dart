22e01525de5255b3c1d05445467c141f5d96c24e
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-07-03 04:45:24 +0200

    Reset password

---
+++ lib/ResetPass.dart
@@ -0,0 +1,161 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:likee_app/signup.dart';
+
+import 'Widget/bezierContainer.dart';
+
+class Resetpas extends StatefulWidget {
+  Resetpas({Key ?key}) : super(key: key);
+
+
+  @override
+  _ResetpasState createState() => _ResetpasState();
+}
+
+class _ResetpasState extends State<Resetpas> {
+
+  String email = '';
+  String warning = '';
+  final _formKey = GlobalKey<FormState>();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  void submit() async {
+    await auth.sendPasswordResetEmail(email: email);
+  }
+  Widget _submitButton() {
+    return GestureDetector(
+      onTap: () async {
+
+      if (_formKey.currentState!.validate()) {
+        setState(() {
+          warning =
+          "A password reset link has been sent to $email";
+        });
+        submit();
+        print('reset');
+      }
+
+
+  },
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.symmetric(vertical: 15),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.grey.shade200,
+                  offset: Offset(2, 4),
+                  blurRadius: 5,
+                  spreadRadius: 2)
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Color(0xff1a84b8), Color(0xff2b6ff7)])),
+        child: Text(
+          'Send',
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'LIK',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.display1,
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Color(0xfffc7b22),
+          ),
+          children: [
+            TextSpan(
+              text: 'EE',
+              style: TextStyle(color: Color(0xff2b6ff7), fontSize: 30),
+            ),
+
+          ]),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Container(
+      height: height,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+              top: -height * .15,
+              right: -MediaQuery.of(context).size.width * .4,
+              child: BezierContainer()),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(height: height * .2),
+                  _title(),
+                  SizedBox(height: 50),
+                  Form(
+                    key: _formKey,
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: EdgeInsets.symmetric(vertical: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  " Type your Email",
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                TextFormField(
+                                    validator: (val) =>
+                                    val!.isEmpty ? 'Enter an email' : null,
+                                    onChanged: (val) {
+                                      setState(() => email = val);
+                                    },
+                                    obscureText: false,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        fillColor: Color(0xfff3f3f4),
+                                        filled: true))
+                              ],
+                            ),
+                          ),
+                        ],
+                      )),
+                  SizedBox(height: 20),
+                  _submitButton(),
+
+
+                ],
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    ));
+  }
+}
