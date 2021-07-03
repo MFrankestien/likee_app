264157167fead8d81b5aa35e138d22bbe8149ebd
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-07-03 04:41:40 +0200

    Login Page page

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -32,7 +32,8 @@ class _LoginPageState extends State<LoginPage> {
           try{
             dynamic result =
             await auth.signInWithEmailAndPassword(email: email.trim(), password: password.toLowerCase());
-
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (context) => RoutePage()));
           }catch(e){
             print(e);
             if (e!=null){
@@ -131,101 +132,107 @@ class _LoginPageState extends State<LoginPage> {
     final height = MediaQuery.of(context).size.height;
     return Scaffold(
         body: Container(
-      height: height,
-      child: Stack(
-        children: <Widget>[
-          Positioned(
-              top: -height * .15,
-              right: -MediaQuery.of(context).size.width * .4,
-              child: BezierContainer()),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: height * .2),
-                  _title(),
-                  SizedBox(height: 50),
-                  Form(
-                    key: _formKey,
-                      child: Column(
-                        children: [
-                      Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "Email",
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                          validator: (val) =>
-                          val!.isEmpty ? 'Enter an email' : null,
-                            onChanged: (val) {
-                              setState(() => email = val);
-                            },
-                            obscureText: false,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                fillColor: Color(0xfff3f3f4),
-                                filled: true))
-                      ],
-                    ),
-                  ),
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 10),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  "Password",
-                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          height: height,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                  top: -height * .15,
+                  right: -MediaQuery.of(context).size.width * .4,
+                  child: BezierContainer()),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: height * .2),
+                      _title(),
+                      SizedBox(height: 50),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "Email",
+                                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    TextFormField(
+                                        validator: (val) =>
+                                        val!.isEmpty ? 'Enter an email' : null,
+                                        onChanged: (val) {
+                                          setState(() => email = val);
+                                        },
+                                        obscureText: false,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            fillColor: Color(0xfff3f3f4),
+                                            filled: true))
+                                  ],
                                 ),
-                                SizedBox(
-                                  height: 10,
+                              ),
+                              Container(
+                                margin: EdgeInsets.symmetric(vertical: 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      "Password",
+                                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    TextFormField(
+                                        validator: (val) => val!.length < 6
+                                            ? 'Enter a password 6+ chars long'
+                                            : null,
+                                        onChanged: (val) {
+                                          setState(() => password = val);
+                                        },
+                                        obscureText: true,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            fillColor: Color(0xfff3f3f4),
+                                            filled: true))
+                                  ],
                                 ),
-                                TextFormField(
-                                    validator: (val) => val!.length < 6
-                                        ? 'Enter a password 6+ chars long'
-                                        : null,
-                                    onChanged: (val) {
-                                      setState(() => password = val);
-                                    },
-                                    obscureText: true,
-                                    decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        fillColor: Color(0xfff3f3f4),
-                                        filled: true))
-                              ],
-                            ),
-                          ),
-                        ],
-                      )),
-                  SizedBox(height: 20),
-                  _submitButton(),
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 10),
-                    alignment: Alignment.centerRight,
-                    child: Text('Forgot Password ?',
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w500)),
-                  ),
+                              ),
+                            ],
+                          )),
+                      SizedBox(height: 20),
+                      _submitButton(),
+                      GestureDetector(
+                        onTap: ()=>Navigator.push(
+                            context, MaterialPageRoute(builder: (context) => Resetpas())),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.centerRight,
+                          child: Text('Forgot Password ?',
+                              style: TextStyle(
+                                  fontSize: 14, fontWeight: FontWeight.w500)),
+                        ),
+                      ),
 
-                  SizedBox(height: height * .055),
-                  _createAccountLabel(),
-                ],
+
+                      SizedBox(height: height * .055),
+                      _createAccountLabel(),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
 
-        ],
-      ),
-    ));
+            ],
+          ),
+        ));
   }
 }
+
